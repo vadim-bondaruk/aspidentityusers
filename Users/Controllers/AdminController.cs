ad4f2e09e2c0c18c780971120aee692a60fe5341
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-06-27 20:44:05 +0300

    + added create user logic and view

--- Users/Controllers/AdminController.cs
+++ Users/Controllers/AdminController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Users.Infrastructure;
+using Users.Models;
 
 namespace Users.Controllers
 {
@@ -14,6 +17,39 @@ namespace Users.Controllers
         public ActionResult Index()
         {
             return View(UserManager.Users.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    this.RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error);
+            }
         }
 
         private AppUserManager UserManager
